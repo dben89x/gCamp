cb09ded46aaa406403beb7638d789bc8c3ab5235
Doug Bennett <dben89x@gmail.com>
2014-10-15 14:13:03 -0600

    made quote objects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,25 @@
 class PagesController < ApplicationController
 
   def home
-    @quotes = ['"If you judge a fish by its ability to climb a tree, it will live its whole life believing that it
-          is stupid." ― Albert Einstein',
-            '“Be yourself; everyone else is already taken.” ― Oscar Wilde',
-            '"Don\'t walk behind me; I may not lead. Don\'t walk in front of me; I may not follow. Just walk
-          beside me and be my friend.” ― Albert Camus']
+
+    @quotes = []
+
+    quote1 = Quote.new
+    quote1.text = '"If you judge a fish by its ability to climb a tree, it will live its whole life believing that
+          it is stupid."'
+    quote1.author = ' ― Albert Einstein'
+
+    quote2 = Quote.new
+    quote2.text = '“Be yourself; everyone else is already taken.”'
+    quote2.author = ' ― Oscar Wilde'
+
+    quote3 = Quote.new
+    quote3.text = '"Don\'t walk behind me; I may not lead. Don\'t walk in front of me; I may not follow. Just walk
+          beside me and be my friend.”'
+    quote3.author =  '― Albert Camus'
+
+    @quotes = [quote1,quote2, quote3]
+
   end
 
 end
