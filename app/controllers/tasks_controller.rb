52bf1eab33dc75fcc4f98b2a64f94edfc7d41039
Doug Bennett <dben89x@gmail.com>
2014-10-23 17:58:22 -0600

    added param links, sorted listings by completion and due date, threw alerts into tasks_helper

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,11 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    if params[:sort_by] == "all"
+      @tasks = Task.all
+    else
+      @tasks = Task.where(complete: false)
+    end
   end
 
   # GET /tasks/1
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,30 @@
 module TasksHelper
+  def due_in(task)
+
+    if task.due_date != nil
+     due_in = (task.due_date - Date.today).to_int
+
+    else
+     due_in = 0.1
+
+    end
+  end
+
+  def row_class_for_task(task, due_in)
+    "opacity:0.5"
+
+    if task.due_date != nil && due_in > 0 && due_in <= 7
+      "alert alert-warning"
+
+    elsif due_in !=  nil && due_in <= 0
+      "alert alert-danger"
+
+    elsif task.due_date != nil && due_in >= 0
+      "alert alert-success"
+
+    elsif task.due_date == nil
+      "none"
+    end
+  end
+
 end
