17930794f6c1b13110ce4b59472b521e55869785
Doug Bennett <dben89x@gmail.com>
2014-11-12 14:27:15 -0700

    slightly separated tests

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 feature 'Tasks' do
 
   scenario 'User creates and edits an event task' do
-
     visit tasks_path
     click_on "Create Task"
+
     fill_in "Description", with: "A description"
     fill_in "Due date", with: "25/4/2020"
     click_on "Create Task"
@@ -15,26 +15,39 @@ feature 'Tasks' do
     click_on "Edit"
     fill_in "Description", with: "Another description"
     fill_in "Due date", with: "26/4/2021"
-    click_on "Update"
-    expect(page).to have_content("Another description")
-
-    click_on "Back"
-    click_on "Show"
-    expect(page).to have_content("Another description")
-    click_on "Edit"
-    fill_in "Description", with: ""
-    fill_in "Due date", with: ""
-    click_on "Update"
-    expect(page).to have_content("Description can't be blank")
-    expect(page).to have_content("Due date can't be blank")
-    fill_in "Due date", with: "1/1/1990"
-    click_on "Update"
-    expect(page).to have_content("Task due date must be after today")
-
     check "Complete"
     click_on "Update"
+    expect(page).to have_content("Another description")
+
     click_on "Back"
+    expect(page).to have_no_content("Another description")
+
     click_on "All"
+    expect(page).to have_content("Another description")
+    expect(page).to have_content("4/26/2021")
+
+    click_on "Show"
+    expect(page).to have_content("Another description")
+  end
+
+  scenario 'User enters invalid fields, then destroys task' do
+    visit new_task_path
+
+    fill_in "Description", with: ""
+    fill_in "Due date", with: ""
+    click_on "Create"
+    expect(page).to have_content("Description can't be blank")
+    expect(page).to have_content("Due date can't be blank")
+
+    fill_in "Due date", with: "1/1/1990"
+    click_on "Create"
+    expect(page).to have_content("Task due date must be after today")
+
+    fill_in "Description", with: "Another description"
+    fill_in "Due date", with: "26/4/2021"
+    click_on "Create"
+    click_on "Back"
+
     expect(page).to have_content("Another description")
     expect(page).to have_content("4/26/2021")
 
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,10 +2,42 @@ require 'rails_helper'
 
 feature 'Users' do
 
-  scenario 'User creates, edits, and deletes user' do
+  User.create(
+    first_name: 'John',
+    last_name: 'Smith',
+    email: 'j@s.com',
+    password: 'js'
+  )
 
+  scenario 'User creates and edits user with wrong and correct info' do
     visit users_path
     click_on "New User"
+
+    fill_in "First name", with: "Joe"
+    fill_in "Last name", with: "Schmoe"
+    fill_in "Email", with: "joe@s.com"
+    fill_in "Password", with: "4321"
+    fill_in "Password confirmation", with: "4321"
+    click_on "Create User"
+    expect(page).to have_content("User was successfully created")
+    expect(page).to have_content("Joe Schmoe")
+    expect(page).to have_content("joe@s.com")
+
+    click_on "Edit", match: :first
+
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: ""
+    click_on "Update User"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+  end
+
+  scenario 'User enters duplicate email' do
+    visit new_user_path
 
     fill_in "First name", with: "Joe"
     fill_in "Last name", with: "Schmoe"
@@ -13,41 +45,33 @@ feature 'Users' do
     fill_in "Password", with: "4321"
     fill_in "Password confirmation", with: "4321"
     click_on "Create User"
-    expect(page).to have_content("User was successfully created")
+    expect(page).to have_content("Email has already been taken")
+  end
 
-    click_on "Edit"
-
-    fill_in "First name", with: ""
-    click_on "Update User"
-    expect(page).to have_content("First name can't be blank")
-    expect(page).to have_content("Password can't be blank")
-
-    fill_in "Last name", with: ""
-    click_on "Update User"
-    expect(page).to have_content("Last name can't be blank")
-
-    fill_in "Email", with: ""
-    click_on "Update User"
-    expect(page).to have_content("Email can't be blank")
+  scenario 'User enters wrong password' do
+    visit new_user_path
 
     fill_in "First name", with: "Jane"
     fill_in "Last name", with: "Plain"
     fill_in "Email", with: "j@p.com"
     fill_in "Password", with: "jp"
     fill_in "Password confirmation", with: "j"
-    click_on "Update User"
+    click_on "Create User"
     expect(page).to have_content("Password confirmation doesn't match Password")
 
     fill_in "Password", with: "jp"
     fill_in "Password confirmation", with: "jp"
-    click_on "Update User"
+    click_on "Create User"
 
-    expect(page).to have_content("User was successfully updated")
+    expect(page).to have_content("User was successfully created")
+  end
 
-    click_on "Edit"
+  scenario 'User deletes user' do
+    visit users_path
+
+    click_on "Edit", match: :first
     click_on "Delete User"
     expect(page).to have_content("User was successfully deleted")
-
   end
 
 end
