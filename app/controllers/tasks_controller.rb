34d5d014aa18c4992ec51db3468e96f8ad76eb20
Doug Bennett <dben89x@gmail.com>
2014-11-17 16:06:50 -0700

    added stuff to project show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -60,7 +60,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: 'Task was successfully destroyed.'
+    redirect_to project_tasks_path(@project), notice: 'Task was successfully destroyed.'
   end
 
 
