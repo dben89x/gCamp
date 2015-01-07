e5d162c89401de10fc0660d8ef82227c3e4e5ad6
Doug Bennett <dben89x@gmail.com>
2015-01-07 16:32:44 -0700

    trying again

--- app/models/tracker_api.rb
+++ app/models/tracker_api.rb
@@ -7,7 +7,6 @@ class TrackerAPI
     response = @conn.get do |req|
       req.url "/services/v5/projects"
       req.headers['Content-Type'] = 'application/json'
-      req.headers['X-TrackerToken'] = token
     end
     JSON.parse(response.body, symbolize_names: true)
   end
